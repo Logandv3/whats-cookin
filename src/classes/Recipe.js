54c427ffd3548b5a16b12115235f1a9984de3546
Logan Vincent <ldvsimp@gmail.com>
2021-08-28 18:12:15 -0600

    Added array of ingredient ids in getIngredientNames

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,3 +1,5 @@
+const ingredient = require('./Ingredient');
+
 class Recipe {
   constructor(recipeData) {
     this.id = recipeData.id;
@@ -6,19 +8,34 @@ class Recipe {
     this.instructions = recipeData.instructions;
     this.name = recipeData.name;
     this.tags = recipeData.tags;
-  }
+  };
 
   getIngredientNames() {
+    let ingredientIds = [];
+    this.ingredients.forEach((ingredient) => {
+      ingredientIds.push(ingredient.id);
+    });
+    let ingredientsForRecipe = ingredientData.filter((id) => ingredient.id === id)
+    return ingredientsObjects;
+// Needs to go in to the recipe.js file and find the
+// ingredients that are used in the recipe and return them.
 
-  }
+
+// Search the ingredientsData file for each of the ingredient
+//   id's, then grab the name and put that in an array.
+
+// Return the array
+
+
+  };
 
   getIngredientCosts() {
 
-  }
+  };
 
   getRecipeInstructions() {
     return this.instructions;
-  }
-}
+  };
+};
 
 export default Recipe;
